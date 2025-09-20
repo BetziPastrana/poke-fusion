898b6d1c6d4d1199e28e7dcded900505730e375c
Betzi Pastrana <pastranabetzi@gmail.com>
2025-09-20 03:01:22 -0600

    Updated

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -22,20 +22,20 @@ export class PokemonListComponent {
     ids.map(id => this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${id}`).toPromise())
   );
 
-this.fusion = {
-  name: pokemons.map(p => p.name).join('-'),
-  types: [...new Set(pokemons.flatMap(p => p.types.map((t: any) => t.type.name)))],
-  stats: pokemons[0].stats.reduce((acc: any, stat: any, i: number) => {
-    acc[stat.stat.name] = Math.floor(
-      (pokemons[0].stats[i].base_stat + pokemons[1].stats[i].base_stat + pokemons[2].stats[i].base_stat) / 3
-    );
-    return acc;
-  }, {}),
-  moves: pokemons.flatMap(p => p.moves.slice(0, 1).map((m: any) => m.move.name)).slice(0, 2),
-  images: pokemons.map(p => p.sprites.front_default) // imágenes de los Pokémon
-};
 
-  }
+  this.fusion = {
+    name: pokemons.map(p => p.name).join('-'),
+    types: [...new Set(pokemons.flatMap(p => p.types.map((t: any) => t.type.name)))],
+    stats: pokemons[0].stats.reduce((acc: any, stat: any, i: number) => {
+      acc[stat.stat.name] = Math.floor(
+        (pokemons[0].stats[i].base_stat + pokemons[1].stats[i].base_stat + pokemons[2].stats[i].base_stat) / 3
+      );
+      return acc;
+    }, {}),
+    moves: pokemons.flatMap(p => p.moves.slice(0, 1).map((m: any) => m.move.name)).slice(0, 2),
+    images: pokemons.map(p => p.sprites.front_default) // agregamos las imágenes
+  };
+}
 
   addToFavorites() {
     if (this.fusion) {
